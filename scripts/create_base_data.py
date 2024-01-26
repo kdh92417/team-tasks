ced979ccb62764c9c2142c6f341075a24e5c8245
daehwi <kdh92417@gmail.com>
2024-01-26 16:04:01 +0900

    파일이 직접 실행될떄만 실행될 수 있도록 스크립트 수정 : 팀 및 유저 스크립트 파일이 직접 실행될때만 스크립트가 실행되도록 수정

--- scripts/create_base_data.py
+++ scripts/create_base_data.py
@@ -1,18 +1,24 @@
 # python3 manage.py shell < ./scripts/create_base_data.py
 
 from django.db import transaction
+
 from app.models import Team, User
+from app.constants.task import TEAM_NAMES
 
-TEAM_NAMES = ["단비", "다래", "블라블라", "철로", "땅이", "해태", "수피"]
 
-with transaction.atomic():
-    for team_name in TEAM_NAMES:
-        team = Team.objects.create(
-            team_name=team_name
-        )
+def create_team_and_user(team_names):
+    with transaction.atomic():
+        for team_name in TEAM_NAMES:
+            team = Team.objects.create(
+                team_name=team_name
+            )
 
-        User.objects.create(
-            team=team,
-            user_name=team.team_name + "-" + "유저",
-            password="1234"
-        )
+            User.objects.create(
+                team=team,
+                user_name=team.team_name + "-" + "유저",
+                password="1234"
+            )
+
+
+if __name__ == "__main__":
+    create_team_and_user(TEAM_NAMES)
