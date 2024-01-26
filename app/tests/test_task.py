aa2d7ebbd65a05da680d65b3eaaa3703095059a0
daehwi <kdh92417@gmail.com>
2024-01-26 14:28:46 +0900

    업무 베이스 테스트케이스 작성 : 테스트 해야될 기본 케이스 유형 작성

---
+++ app/tests/test_task.py
@@ -0,0 +1,30 @@
+from rest_framework.test import APITestCase
+
+
+class TaskTests(APITestCase):
+    def setUp(self):
+        pass
+
+    def test_success_create_task(self):
+        pass
+
+    def test_success_get_task(self):
+        pass
+
+    def test_success_modify_task(self):
+        pass
+
+    def test_success_get_sub_task(self):
+        pass
+
+    def test_success_modify_sub_task(self):
+        pass
+
+    def test_success_complete_task_by_task_creator(self):
+        pass
+
+    def test_success_auto_complete_task(self):
+        pass
+
+    def test_success_complete_sub_task_by_sub_team(self):
+        pass
