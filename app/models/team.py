e12364c43c6427caef00db63f72aa8781524c4fd
daehwi <kdh92417@gmail.com>
2024-01-28 15:07:07 +0900

    유효한 팀여부확인을 위한 "is_verified" 컬럼 추가

--- app/models/team.py
+++ app/models/team.py
@@ -5,6 +5,7 @@ from app.models.base import DateModel
 
 class Team(DateModel):
     team_name = models.CharField("팀 이름", max_length=255, unique=True)
+    is_verified = models.BooleanField("인증 여부", default=False)
 
     class Meta:
         verbose_name = verbose_name_plural = "팀"
