0d9b629086c81f936e67c947a37358e796a43d9c
daehwi <kdh92417@gmail.com>
2024-01-28 15:49:49 +0900

    업무 API에 Swagger 추가를 위한 extend schema view 작성

--- app/views/task.py
+++ app/views/task.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from rest_framework import status
 from rest_framework.decorators import action
@@ -14,17 +15,29 @@ from app.serializers.task import (
     CreateTaskSerializer,
     CompleteSubTaskSerializer,
     SubTaskSerializer,
+    UpdateTaskSerializer,
+    CreateSubTaskSerializer,
 )
 
 from app.view import CreateModelMixin, UpdateModelMixin
 
 
+@extend_schema_view(
+    create=extend_schema(summary="업무 생성", request=CreateTaskSerializer),
+    list=extend_schema(
+        summary="업무 리스트 열람.",
+    ),
+    partial_update=extend_schema(summary="업무 내용 변경", request=UpdateTaskSerializer),
+    retrieve=extend_schema(exclude=True),
+    update=extend_schema(exclude=True),
+    destroy=extend_schema(exclude=True),
+)
 class TaskViewSet(ModelViewSet, CreateModelMixin, UpdateModelMixin):
     permission_classes = [IsAuthenticated]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     create_serializer_class = CreateTaskSerializer
-    update_serializer_class = CreateTaskSerializer
+    update_serializer_class = UpdateTaskSerializer
 
     def get_queryset(self):
         user = self.request.user
@@ -34,10 +47,33 @@ class TaskViewSet(ModelViewSet, CreateModelMixin, UpdateModelMixin):
 
         return queryset
 
+
+@extend_schema_view(
+    create=extend_schema(summary="하위 업무 부여", request=CreateSubTaskSerializer),
+    destroy=extend_schema(summary="특정 하위 업무 삭제"),
+    partial_update=extend_schema(exclude=True),
+    retrieve=extend_schema(exclude=True),
+    update=extend_schema(exclude=True),
+    list=extend_schema(exclude=True),
+)
+class SubTaskViewSet(ModelViewSet):
+    permission_classes = [IsCreator]
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        sub_task = self.get_object()
+        if sub_task.is_complete:
+            raise ValidationError("완료된 하위업무는 삭제할 수 없습니다.")
+        return super().destroy(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="하위 업무 완료 처리.",
+    )
     @action(
         methods=["post"],
         detail=True,
-        url_path="sub-tasks/completion",
+        url_path="completion",
         url_name="completion",
     )
     def complete_task(self, request, *args, **kwargs):
@@ -54,15 +90,3 @@ class TaskViewSet(ModelViewSet, CreateModelMixin, UpdateModelMixin):
         sub_task_serializer.update(sub_task, {})
 
         return Response({"message": "업무가 정상적으로 완료처리 되었습니다."}, status=status.HTTP_200_OK)
-
-
-class SubTaskViewSet(ModelViewSet):
-    permission_classes = [IsCreator]
-    queryset = SubTask.objects.all()
-    serializer_class = SubTaskSerializer
-
-    def destroy(self, request, *args, **kwargs):
-        sub_task = self.get_object()
-        if sub_task.is_complete:
-            raise ValidationError("완료된 하위업무는 삭제할 수 없습니다.")
-        return super().destroy(request, *args, **kwargs)
