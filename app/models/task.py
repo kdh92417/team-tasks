47d6c3f540239385e382c2d8af0ac77abadb1891
daehwi <kdh92417@gmail.com>
2024-01-28 15:08:13 +0900

    유니크 제약 및 SubTask 와 Task 관계설정 수정

--- app/models/task.py
+++ app/models/task.py
@@ -5,6 +5,26 @@ from app.models.team import Team
 from app.models.user import User
 
 
+class SubTask(DateModel):
+    task = models.ForeignKey(
+        verbose_name="업무",
+        to="app.Task",
+        on_delete=models.CASCADE,
+        related_name="sub_tasks",
+    )
+    team = models.ForeignKey(
+        verbose_name="하위업무-팀", to=Team, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    is_complete = models.BooleanField("완료 여부", default=False)
+    completed_date = models.DateTimeField("완료 날짜", null=True, blank=True)
+
+    class Meta:
+        verbose_name = verbose_name_plural = "하위 업무"
+        constraints = [
+            models.UniqueConstraint(fields=["task", "team"], name="unique_task_team")
+        ]
+
+
 class Task(DateModel):
     create_user = models.ForeignKey(
         verbose_name="업무-유저", to=User, on_delete=models.SET_NULL, null=True, blank=True
@@ -20,21 +40,3 @@ class Task(DateModel):
 
     class Meta:
         verbose_name = verbose_name_plural = "업무"
-
-
-class SubTask(DateModel):
-    is_complete = models.BooleanField("완료 여부", default=False)
-    completed_date = models.DateTimeField("완료 날짜", null=True, blank=True)
-
-    class Meta:
-        verbose_name = verbose_name_plural = "하위 업무"
-
-
-class SubTaskTeamAssociation(DateModel):
-    subtask = models.ForeignKey(
-        verbose_name="하위업무", to=SubTask, on_delete=models.CASCADE
-    )
-    team = models.ForeignKey(verbose_name="팀", to=Team, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = verbose_name_plural = "하위업무-팀-연관정보"
