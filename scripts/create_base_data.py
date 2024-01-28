cf00fb6022a3cf87c23312f7962b92f8982d9bfa
daehwi <kdh92417@gmail.com>
2024-01-28 15:04:48 +0900

    초기 데이터 생성 스크립트 수정

--- scripts/create_base_data.py
+++ scripts/create_base_data.py
@@ -9,14 +9,10 @@ from app.constants.task import TEAM_NAMES
 def create_team_and_user(team_names):
     with transaction.atomic():
         for team_name in TEAM_NAMES:
-            team = Team.objects.create(
-                team_name=team_name
-            )
+            team = Team.objects.create(team_name=team_name, is_verified=True)
 
             User.objects.create(
-                team=team,
-                user_name=team.team_name + "-" + "유저",
-                password="1234"
+                team=team, user_name=team.team_name + "-" + "유저", password="1234"
             )
 
 
